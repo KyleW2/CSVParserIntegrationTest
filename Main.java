df3d1de88827104829c66581274d6f05df9f4225
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-01-23 21:09:11 -0600

    Created new validater class and main

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CSVValidater test = new CSVValidater("valid.csv");
+        System.out.println(test.getCSVString());
+    }
+}
