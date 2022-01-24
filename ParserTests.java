bf81c556e9f22185ec2dac21e108c591dd91b99f
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-01-24 14:24:41 -0600

    Added testing

---
+++ ParserTests.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class ParserTests {
+    @Test
+    public void testAssertEquals() {
+      assertEquals("failure - strings are not equal", "text", "text");
+    }
+}
