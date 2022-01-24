a00d0321a23807cb3def6b55132576830bcc959a
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-01-23 23:34:04 -0600

    Added more comments

--- CSVParser.java
+++ CSVParser.java
@@ -6,17 +6,21 @@ import java.util.ArrayList;
 public class CSVParser {
     File csvFile;
 
+    // Constructor
     public CSVParser(String fileName) {;
         csvFile = new File(fileName);
     }
 
-    // Reads lines of csv file into an ArrayList for getCSV()
+    // Reads lines/rows of csv file into an ArrayList for getCSV()
     private ArrayList<String> readLines() {
+        // ArrayList of rows to return
         ArrayList<String> lines = new ArrayList<String>();
 
+        // Try catch for FileNotFound
         try {
             Scanner scan = new Scanner(csvFile);
-
+            
+            // Adds each row of the csv to the ArrayList
             while(scan.hasNextLine()) {
                 lines.add(scan.nextLine());
             }
@@ -33,10 +37,12 @@ public class CSVParser {
 
     // Splits lines from readLine() into 2d array
     public ArrayList<String[]> getCSV() {
+        // 2D array to return
         ArrayList<String[]> csv = new ArrayList<String[]>();
+        // ArrayList to hold rows
         ArrayList<String> lines = readLines();
 
-        // For each line from readLine() split by "," and add to ArrayList
+        // Split each row by "," into an array of columns
         for(int i = 0; i < lines.size(); i++){
             csv.add(lines.get(i).split(",", 0));
         }
@@ -46,10 +52,14 @@ public class CSVParser {
 
     // Just for printing
     public String getCSVString() {
+        // Return string
         String out = new String();
+        // CSV ArrayList
         ArrayList<String[]> csv = getCSV();
 
+        // Loop thru rows
         for(int i = 0; i < csv.size(); i++) {
+            // Loop thru columns in the row
             for(int j = 0; j < csv.get(i).length; j++) {
                 out = out + csv.get(i)[j] + " ";
             }
