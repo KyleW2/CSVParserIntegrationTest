dfda26c36131c040e20e2ad565df80b0a02f3b90
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-01-24 19:38:37 -0600

    Refactored again

--- CSVParser/src/test/java/com/CapstoneTeam18/app/AppTest.java
+++ CSVParser/src/test/java/com/CapstoneTeam18/app/AppTest.java
@@ -1,15 +1,7 @@
 package com.capstoneteam18.app;
 
 import org.junit.Test;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNotSame;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertSame;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 
