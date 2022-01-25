e3b8125a7f82a48dd531414aae7d819b2e5e011f
Kyle <Kyle.J.Weidner-1@ou.edu>
2022-01-24 19:55:47 -0600

    Fixing maven action

--- CSVParser/src/test/java/com/CapstoneTeam18/app/AppTest.java
+++ CSVParser/src/test/java/com/CapstoneTeam18/app/AppTest.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 public class AppTest {
     @Test
     public void testRandomElements() {
-      CSVParser test = new CSVParser("valid.csv");
+      CSVParser test = new CSVParser("/home/runner/work/Capstone/Capstone/CSVParser/src/test/java/com/capstoneteam18/app/valid.csv");
       ArrayList<String[]> testCsv = test.getCSV();
       assertEquals("failure - element is not correct", "1", testCsv.get(1)[1]);
       assertEquals("failure - element is not correct", "d", testCsv.get(4)[0]);
@@ -29,7 +29,7 @@ public class AppTest {
 
     @Test
     public void testNumberOfRows() {
-      CSVParser test = new CSVParser("valid.csv");
+      CSVParser test = new CSVParser("/home/runner/work/Capstone/Capstone/CSVParser/src/test/java/com/capstoneteam18/app/valid.csv");
       assertEquals("failure - number of rows are not equal", 6, test.getNumRows());
     }
 }
